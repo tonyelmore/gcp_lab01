34719c3ccdbee052c61accb909fbcaa69c1b70b3
Anthony Elmore <telmore@pivotal.io>
2018-12-18 17:03:45 -0700

    consistent name for lab01

--- generateCerts.sh
+++ generateCerts.sh
@@ -1,8 +1,8 @@
 credhub generate -t certificate \
   -n /concourse/main/gcp-lab01-cert \
   -c differentscale.com \
-  -a *.system.lab01.differentscale.com \
-  -a *.apps.lab01.differentscale.com \
-  -a *.login.system.lab01.differentscale.com \
-  -a *.uaa.system.lab01.differentscale.com \
+  -a *.system.gcplab01.differentscale.com \
+  -a *.apps.gcplab01.differentscale.com \
+  -a *.login.system.gcplab01.differentscale.com \
+  -a *.uaa.system.gcplab01.differentscale.com \
   --self-sign
--- pipelines/InstallPCF/flyme.sh
+++ pipelines/InstallPCF/flyme.sh
@@ -2,4 +2,4 @@ fly -t gcp \
 	set-pipeline \
 	-p gcplab01 \
 	-c base_pipeline.yml \
-	-l ../../foundations/lab01/pipeline-params.yml
+	-l ../../foundations/gcplab01/pipeline-params.yml
