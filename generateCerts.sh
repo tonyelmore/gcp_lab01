b7fffc826d6064f1cf7473f14c3496f506d4e656
Anthony Elmore <telmore@pivotal.io>
2019-01-02 14:54:31 -0600

    missing parms

--- generateCerts.sh
+++ generateCerts.sh
@@ -6,3 +6,18 @@ credhub generate -t certificate \
   -a *.login.system.lab01.telmore.io \
   -a *.uaa.system.lab01.telmore.io \
   --self-sign
+
+credhub generate -n /concourse/main/networking_cert \
+                -t certificate \
+                --self-sign \
+                -c *.system.lab01.telmore.io \
+                -a *.system.lab01.telmore.io \
+                -a *.apps.lab01.telmore.io \
+                -a *.login.system.lab01.telmore.io \
+                -a *.uaa.system.lab01.telmore.io
+
+credhub generate -n /concourse/main/uaa_cert \
+                 -t certificate \
+                 --self-sign \
+                 -c *.login.system.lab01.telmore.io \
+                 -a *.login.system.lab01.telmore.io
