cc6bb861acf97ce0f7f34a0e2c25542e58a705bf
Anthony Elmore <telmore@pivotal.io>
2019-01-01 22:56:48 -0600

    forgot line continuation

--- pipelines/InstallPCF/flyme.sh
+++ pipelines/InstallPCF/flyme.sh
@@ -2,5 +2,5 @@ fly -t gcp \
 	set-pipeline \
 	-p install-lab01 \
 	-c pipeline_final.yml \
-	-l ../../foundations/lab01/pipeline-params.yml
+	-l ../../foundations/lab01/pipeline-params.yml \
 	-l ../../common-config/opsman.yml
