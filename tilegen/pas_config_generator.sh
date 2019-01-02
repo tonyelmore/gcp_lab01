bbf08c974f8aa16e2cee32308931bbb65e67ee10
Anthony Elmore <telmore@pivotal.io>
2019-01-01 21:42:36 -0600

    changes to support refactored structure

--- tilegen/pas_config_generator.sh
+++ tilegen/pas_config_generator.sh
@@ -1,8 +1,13 @@
+product_slug="elastic-runtime"
+product_version="2.2.10"
+product_glob="cf*.pivotal"
+
 pivnet_token=$(credhub get -n /concourse/main/pivnet_token -j | jq -r .value)
+
 tile-config-generator generate \
     --base-directory=. \
     --include-errands \
     --token=$pivnet_token \
-    --product-slug=elastic-runtime \
-    --product-version=2.2.10 \
-    --product-glob=cf*.pivotal
+    --product-slug=$product_slug \
+    --product-version=$product_version \
+    --product-glob=$product_glob
--- tilegen/pas_set_tile_properties.sh
+++ tilegen/pas_set_tile_properties.sh
@@ -9,3 +9,15 @@ bosh int $productName/$productVersion/product.yml \
     > ../foundations/$env/products/$productName/$productName.yml
 
 touch -a ../foundations/$env/products/$productName/$productName-vars.yml
+
+SHELL_OUTPUT="../foundations/$env/products/$productName/$productName-interpolate.sh"
+
+/bin/cat <<EOM >$SHELL_OUTPUT
+bosh int \\
+    $productName.yml \\
+    -l ../../../../tilegen/$productName/$productVersion/errand-vars.yml \\
+    -l ../../../../tilegen/$productName/$productVersion/resource-vars.yml \\
+    -l ../../../../tilegen/$productName/$productVersion/product-default-vars.yml \\
+    -l $productName-vars.yml \\
+    --var-errs
+EOM
