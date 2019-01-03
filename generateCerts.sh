dd18e81b60df33e58596d953156ac5ae20deb785
Anthony Elmore <telmore@pivotal.io>
2019-01-03 15:16:37 -0600

    change system to sys for domain name

--- generateCerts.sh
+++ generateCerts.sh
@@ -1,23 +1,23 @@
 credhub generate -t certificate \
   -n /concourse/main/gcp-lab01-cert \
   -c telmore.io \
-  -a *.system.lab01.telmore.io \
+  -a *.sys.lab01.telmore.io \
   -a *.apps.lab01.telmore.io \
-  -a *.login.system.lab01.telmore.io \
-  -a *.uaa.system.lab01.telmore.io \
+  -a *.login.sys.lab01.telmore.io \
+  -a *.uaa.sys.lab01.telmore.io \
   --self-sign
 
 credhub generate -n /concourse/main/networking_cert \
                 -t certificate \
                 --self-sign \
-                -c *.system.lab01.telmore.io \
-                -a *.system.lab01.telmore.io \
+                -c *.sys.lab01.telmore.io \
+                -a *.sys.lab01.telmore.io \
                 -a *.apps.lab01.telmore.io \
-                -a *.login.system.lab01.telmore.io \
-                -a *.uaa.system.lab01.telmore.io
+                -a *.login.sys.lab01.telmore.io \
+                -a *.uaa.sys.lab01.telmore.io
 
 credhub generate -n /concourse/main/uaa_cert \
                  -t certificate \
                  --self-sign \
-                 -c *.login.system.lab01.telmore.io \
-                 -a *.login.system.lab01.telmore.io
+                 -c *.login.sys.lab01.telmore.io \
+                 -a *.login.sys.lab01.telmore.io
