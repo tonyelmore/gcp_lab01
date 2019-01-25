c7e1b117a354cc252f77b9c012f8c0c118ead3e7
Anthony Elmore <telmore@pivotal.io>
2019-01-25 08:34:28 -0600

    fix scripts

--- opsman_lab01.sh
+++ opsman_lab01.sh
@@ -1,6 +1,13 @@
-export OM_PASSWORD=opsmanpassword
 export OM_USERNAME=opsman
 export OM_TARGET=pcf.lab01.telmore.io
+
+unset OM_PASSWORD
+
+read -e -p "Enter OPSMAN Password: " PWD
+export OM_PASSWORD=$PWD
+
+eval "$(om -k bosh-env -i ~/.ssh/lab01-gcp-opsman)"
+
 
 BOSH_COMMAND_LINE=$(om -k curl -s -p /api/v0/deployed/director/credentials/bosh_commandline_credentials | jq -r .credential)
 echo $BOSH_COMMAND_LINE | tr -d '\n' | pbcopy
