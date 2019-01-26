6a9d4fcf41860bc2f8d0d33e9fedc111381d9ca1
Anthony Elmore <telmore@pivotal.io>
2019-01-26 11:46:33 -0600

    getting APM set up right

--- foundations/lab01/products/apm/apm-interpolate.sh
+++ foundations/lab01/products/apm/apm-interpolate.sh
@@ -1,7 +1,7 @@
 bosh int \
-    apmPostgres.yml \
+    apm.yml \
     -l ../../../../tilegen/apmPostgres/1.6.0-build.41/errand-vars.yml \
     -l ../../../../tilegen/apmPostgres/1.6.0-build.41/resource-vars.yml \
     -l ../../../../tilegen/apmPostgres/1.6.0-build.41/product-default-vars.yml \
-    -l apmPostgres-vars.yml \
+    -l apm-vars.yml \
     --var-errs
--- tilegen/apm_set_tile_properties.sh
+++ tilegen/apm_set_tile_properties.sh
@@ -1,11 +1,16 @@
-productName="apmPostgres"
+#### NOTE ####
+# Do NOT use this as a template to start another product
+# the slug and the internal name do not match - so this has been hacked
+
+productName="apm"
+tilegenName="apmPostgres"
 productVersion="1.6.0-build.41"
 env="lab01"
 
 mkdir -p ../foundations/$env/products/$productName
 
-bosh int $productName/$productVersion/product.yml \
-    -o $productName/$productVersion/network/3-az-configuration.yml \
+bosh int $tilegenName/$productVersion/product.yml \
+    -o $tilegenName/$productVersion/network/3-az-configuration.yml \
     > ../foundations/$env/products/$productName/$productName.yml
 
 touch -a ../foundations/$env/products/$productName/$productName-vars.yml
@@ -15,9 +20,9 @@ SHELL_OUTPUT="../foundations/$env/products/$productName/$productName-interpolate
 /bin/cat <<EOM >$SHELL_OUTPUT
 bosh int \\
     $productName.yml \\
-    -l ../../../../tilegen/$productName/$productVersion/errand-vars.yml \\
-    -l ../../../../tilegen/$productName/$productVersion/resource-vars.yml \\
-    -l ../../../../tilegen/$productName/$productVersion/product-default-vars.yml \\
+    -l ../../../../tilegen/$tilegenName/$productVersion/errand-vars.yml \\
+    -l ../../../../tilegen/$tilegenName/$productVersion/resource-vars.yml \\
+    -l ../../../../tilegen/$tilegenName/$productVersion/product-default-vars.yml \\
     -l $productName-vars.yml \\
     --var-errs
 EOM
